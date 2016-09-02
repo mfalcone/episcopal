5d44b5a9b292dbc706e2ceda0138f83de0d609b0
user <maxifalcone@gmail.com>
2016-09-02 12:15:58 -0300

    varios

--- episcopal/metas.php
+++ episcopal/metas.php
@@ -1,6 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-<title>Building Community</title>
+<title>Episcopal School of Dallas</title>
 
 <!-- Mobile Specific Metas -->
 <!--meta name="viewport" content="width=device-width, initial-scale=1" /--> 
